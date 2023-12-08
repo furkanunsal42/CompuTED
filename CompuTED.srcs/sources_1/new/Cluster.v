8bbee9983d3c3918373ccd2fe022d67c3c9fdfab
KodBilen42 <89701935+furkanunsal42@users.noreply.github.com>
2023-12-08 05:31:34 +0300

    cluster generation

--- CompuTED.srcs/sources_1/new/Cluster.v
+++ CompuTED.srcs/sources_1/new/Cluster.v
@@ -1,6 +1,20 @@
 
-module Cluster #(parameter CORE_COUNT = 2) ();
+module Cluster #(parameter CORE_COUNT = 2) (clk);
+    input clk;
     
-    Core cores[CORE_COUNT-1:0]();
+    
+    wire[31:0] MemoryRead;
+    wire[31:0] MemoryWrite;
+    wire[31:0] MemoryAddress;
+    wire MemoryWriteFlag;
+    Memory memory_L1(MemoryRead, MemoryAddress, MemoryWrite, MemoryWriteFlag, clk);
+    memory_L1(.MEMORY_SIZE(128));
+    
+    generate 
+        genvar i;
+        for (i = 0; i <= CORE_COUNT-1; i = i + 1) begin
+            Core i_core();
+        end
+    endgenerate
 
 endmodule
